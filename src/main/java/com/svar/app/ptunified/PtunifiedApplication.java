cf3aee2d98d5bdad6776bb16b7438d80bc9e216a
aceraces <stenvaher@gmail.com>
2024-03-12 20:57:10 +0200

    #1 - Initial commit

---
+++ src/main/java/com/svar/app/ptunified/PtunifiedApplication.java
@@ -0,0 +1,13 @@
+package com.svar.app.ptunified;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PtunifiedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PtunifiedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/svar/app/ptunified/PtunifiedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.svar.app.ptunified;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PtunifiedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
